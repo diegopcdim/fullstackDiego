3b72c219b8fb1db3b2d3048f81d2ac6d65ad78d9
diegopcdim <diegopcdim@gmail.com>
2026-06-12 11:27:21 +0200

    Ejercicio #6 (#5)

---
+++ tareas-app/src/app/services/tarea.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tarea } from '../models/tarea';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareaService {
+
+  private apiUrl = 'http://localhost:8080/tareas';
+
+  constructor(private http: HttpClient) {}
+
+  getTareas(): Observable<Tarea[]> {
+    return this.http.get<Tarea[]>(this.apiUrl);
+  }
+}
